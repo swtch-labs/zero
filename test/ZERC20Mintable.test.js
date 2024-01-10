38a8dfd5c655f5a29c60193aac2fc90e260a0e06
Astor Rivera <astordigital@gmail.com>
2024-01-09 17:22:50 -0800

    Completed mint and burn, note: we will need to add access control.

---
+++ test/ZERC20Mintable.test.js
@@ -0,0 +1,35 @@
+const ZERC20Mintable = artifacts.require("ZERC20Mintable");
+
+contract("ZERC20Mintable", accounts => {
+    const [owner, recipient] = accounts;
+    let token;
+
+    beforeEach(async () => {
+        token = await ZERC20Mintable.new("ZeroToken", "ZTK", web3.utils.toWei('1000', 'ether'));
+    });
+
+    it("should mint tokens correctly", async () => {
+        const mintAmount = web3.utils.toWei('100', 'ether');
+        const initialTotalSupply = await token.totalSupply();
+        const initialRecipientBalance = await token.balanceOf(recipient);
+
+        await token.mint(recipient, mintAmount);
+
+        const newTotalSupply = await token.totalSupply();
+        const newRecipientBalance = await token.balanceOf(recipient);
+
+        assert.equal(
+            newTotalSupply.toString(), 
+            initialTotalSupply.add(new web3.utils.BN(mintAmount)).toString(),
+            "Total supply did not increase correctly"
+        );
+
+        assert.equal(
+            newRecipientBalance.toString(), 
+            initialRecipientBalance.add(new web3.utils.BN(mintAmount)).toString(),
+            "Recipient balance did not increase correctly"
+        );
+    });
+
+    // Additional tests can be added here, e.g., testing access control for the mint function
+});
